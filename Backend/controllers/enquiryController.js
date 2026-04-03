1f4a5d54947c7e73576c5fb769b657c50013333f
Hjeandedieu <hashimweyesujeandedie@gmail.com>
2026-04-03 23:09:59 +0200

    Enquiry controller concluded

--- Backend/controllers/enquiryController.js
+++ Backend/controllers/enquiryController.js
@@ -28,10 +28,57 @@ const getEnquiriesById = async (req,res) =>{
 const getEnquiryByUser = async(req, res) => {
     try{
         const {user} = req.query;
-        const {enquiries} = await getEnquiryByUser();
+        const {enquiries} = await getEnquiryByUser(user);
         res.status(200).json(enquiries)
     }catch(err){
         res.status(500).json({message:"Failed to fetch Enquiry"})
+    }
+}
+
+// GET ENQUIRIES BY MODEL
+
+const getEnquiriesByModel = async(req,res) => {
+    try{
+        const {model} = req.query;
+        const {enquiries} = await getEnquiriesByModel(model)
+        res.status(200).json(enquiries)
+    }catch(err){
+        res.status(500).json({message:"Failed to fetch Enquiries"})
+    }
+}
+
+// CREATE ENQUIRY
+
+const createEnquiry = async(req,res)=>{
+    try{
+        const newEnquiryId = await createEnquiry(req.body)
+        res.status(201).json({message:"Car added", id: newEnquiryId})
+    }catch(err){
+        res.status(500).json({message: "Failed to add enquiry"})
+    }
+}
+
+// UPDATE ENQUIRY
+
+const updateEnquiry = async(req,res) => {
+    try{
+        const updated = await updateEnquiry(req.params.id, req.body);
+        if (!updated) return res.status(404).json({message:"Enquiry Not found"})
+        res.status(201).json({message:"Enquiry updated"})
+    }catch(err){
+        res.status(500).json({message: "Failed to update Enquiry"})
+    }
+}
+
+// UPDATE ENQUIRY STATUS
+
+const updateEnquiryStatus = async(req,res) => {
+    try{
+        const updated = await updateEnquiryStatus(req.params.id, req.body);
+        if (!updated) return res.status(404).json({message:"Enquiry not found"})
+        res.status(201).json({message: "Enquiry status updated."})
+    }catch(err){
+        res.status(500).json({message: "Failed to update Enquiry Status"})
     }
 }
 
