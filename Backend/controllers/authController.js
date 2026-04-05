48889d65e2e9cb0ecb852b9d3b72b73ea70cd6a7
Hjeandedieu <hashimweyesujeandedie@gmail.com>
2026-04-05 10:34:08 +0200

    Added logout logic

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -2,6 +2,8 @@ const bcrypt = require("bcrypt")
 const jwt = require("jsonwebtoken")
 const { createNewUser, getUserByEmail} = require("../models/userModel")
 
+
+// SIGNUP
 const signup = async (req, res) => {
     try{
         const {full_name, email, password} = req.body;
@@ -14,7 +16,7 @@ const signup = async (req, res) => {
         // HASH PASSWORD
         const hashedPassword = await bcrypt.hash(password, 10);
 
-        // SAVE NEW USER OT DATABASE
+        // SAVE NEW USER IN DATABASE
         const newUserId = await createNewUser({full_name, email, password: hashedPassword});
 
         // GENERATE JWT TOKEN
@@ -34,4 +36,51 @@ const signup = async (req, res) => {
     }catch(err){
         res.status(500).json({message: "Signup failed."})
     }
+}
+
+// LOGIN
+const login = async(req,res) => {
+    try{
+        const {email, password} = req.body;
+
+        //FIND USER BY EMAIL
+        const user = await getUserByEmail(email);
+        if (!user) return res.status(404).json({message: "User not found"});
+
+        // COMPARE PASSWORDS
+        const passwordMatch = await bcrypt.compare(password, user.password);
+        if(!passwordMatch) return res.status(401).json({message: "Invalid credentials"})
+
+        // GENERATE JWT TOKEN
+        const token = jwt.sign(
+            {id: user.id},
+            process.env.JWT_SECRET,
+            {expiresIn: "7d"}
+        )
+
+        // SEND RESPONSE
+        res.status(200).json({
+            message:"Login successful",
+            token,
+            user: {id: user.id, full_name: user.full_name, email: user.email}
+        })
+
+
+    }catch(err){
+        res.status(500).json({message: "Login failed"})
+    }
+}
+
+// LOGOUT
+
+const logout = async(req,res) => {
+    try{
+        res.status(200).json({message: "Logged out successfully."})
+    }catch(err){
+        res.status(500).json({message: "Logout Failed."})
+    }
+}
+
+const getLoggedInUser = async (req,res) => {
+    
 }
